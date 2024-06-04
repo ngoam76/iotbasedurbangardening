dab7c3abdd9df9efe91ec593df68df7c99ed04e5
numanmuttaqi <numan.muttaqi@tum.de>
2024-06-04 17:26:12 +0200

    Update grid main dashboard

--- DashboardNuman/script.js
+++ DashboardNuman/script.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const closeBtn = document.getElementById('close-btn');
+    const toggleBtn = document.getElementById('toggle-btn');
     const sidebar = document.getElementById('sidebar');
+    const alertBox = document.querySelector('header .alert');
+    const closeAlertBtn = document.getElementById('close-alert');
+    const openAlertBtn = document.getElementById('open-alert');
+    const header = document.querySelector('header');
 
-    closeBtn.addEventListener('click', () => {
+    toggleBtn.addEventListener('click', () => {
         sidebar.classList.toggle('minimized');
+    });
+
+    closeAlertBtn.addEventListener('click', () => {
+        alertBox.classList.add('hide');
+        header.classList.add('hide');
+        openAlertBtn.style.display = 'block';
+    });
+
+    openAlertBtn.addEventListener('click', () => {
+        alertBox.classList.remove('hide');
+        header.classList.remove('hide');
+        openAlertBtn.style.display = 'none';
     });
 });
