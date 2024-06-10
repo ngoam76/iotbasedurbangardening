47b9f742f9b3a800adbc3208bb10f43cc3180414
numanmuttaqi <numan.muttaqi@tum.de>
2024-06-10 15:32:48 +0200

    Update everythinggggg

--- application.py
+++ application.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, request, Response, send_from_directory
 from data_retrieving_frost import get_latest_observations
 from data_retrieving_wf import get_weather
-#export to HTML
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('static/css', path)
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('static/js', path)
 
 @app.route('/frost')
 def frost():
@@ -17,7 +29,7 @@ def frost():
 def weatherforecast():
     api_key = '4890013cad9b0ee94bf0edd0d861d569'
     city_name = 'Munich'
-    return Response(get_weather(api_key, city_name, interval=5), mimetype='text/event-stream')
+    return Response(get_weather(api_key, city_name, interval=60), mimetype='text/event-stream')
     
 if __name__ == '__main__':
     app.run(debug=True, port=5500)
